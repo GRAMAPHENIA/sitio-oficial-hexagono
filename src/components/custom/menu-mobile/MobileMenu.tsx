b5410d3f94205097101f8336813b6f95a8032e16
GRAMAPHENIA <dicoratojuanpablo@gmail.com>
2024-10-16 15:30:09 -0300

    mods

--- src/components/custom/menu-mobile/MobileMenu.tsx
+++ src/components/custom/menu-mobile/MobileMenu.tsx
@@ -9,18 +9,29 @@ import {
 } from "@/components/ui/popover";
 import { MenuMobileItem } from "@/types/menuMobileItem";
 import { menuMobileItems } from "@/data/menuMobileItems";
+import { useState } from "react"; 
 
 const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Popover>
+    <Popover onOpenChange={setIsOpen}>
       <PopoverTrigger>
         <Menu className="h-6 w-6" />
       </PopoverTrigger>
-      <PopoverContent className="p-4 bg-white dark:bg-card">
+
+      <PopoverContent
+        className="p-4 bg-white dark:bg-card"
+        aria-hidden={!isOpen} // Oculta del lector de pantalla cuando no está abierto
+      >
         {menuMobileItems.map((item: MenuMobileItem, index: number) => (
-          <Link key={index} href={item.href} className="block py-2 ">
+          <Link
+            key={index}
+            href={item.href}
+            className="block py-2"
+            tabIndex={isOpen ? 0 : -1} // Impide foco cuando está cerrado
+          >
             <p className="py-2 text-center"> {item.label}</p>
-           
           </Link>
         ))}
       </PopoverContent>
