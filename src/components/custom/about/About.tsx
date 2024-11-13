8e3050b93199ae2da356020c46a8c69794fb156b
GRAMAPHENIA <dicoratojuanpablo@gmail.com>
2024-11-13 16:28:27 -0300

    componente-about

--- src/components/custom/about/About.tsx
+++ src/components/custom/about/About.tsx
@@ -1,89 +1,107 @@
 "use client";
 
-import { vollkorn } from "@/styles/fonts";
+import { exo2, vollkorn } from "@/styles/fonts";
 import Image from "next/image";
-import { User, Globe, Target } from "lucide-react";
+import Link from "next/link";
+import { FaGithub, FaGlobe, FaLinkedin } from "react-icons/fa"; // Todos los iconos de react-icons
+import { RiInstagramFill } from "react-icons/ri";
 
 const AboutUs = () => {
+  const developers = [
+    {
+      name: "Dylan Server",
+      role: "Frontend Developer",
+      image: "/clients/avatar-she-another.webp",
+      description: "React y diseño de interfaces de usuario.",
+      github: "https://github.com/Bird",
+      linkedin: "https://www.linkedin.com/in/mait%C3%A9-de-le%C3%B3n/",
+      instagram: "https://www.instagram.com/birdie.borda/",
+      globe: "https://www.hexagono.xyz/",
+    },
+    {
+      name: "Rojo Server",
+      role: "Frontend Developer",
+      image: "/clients/avatar-he.webp",
+      description: "React y diseño de interfaces de usuario.",
+      github: "https://github.com/GRAMAPHENIA",
+      linkedin: "https://www.linkedin.com/in/dicoratojuanpablo/",
+      instagram: "https://www.instagram.com/gramaphenia/",
+      globe: "https://www.hexagono.xyz/",
+    },
+  ];
+
   return (
     <section className="flex flex-col items-center mx-auto text-center">
-      <div className="mx-auto px-6">
-        {/* Hero Section */}
-        <div className="">
-          <h1
-            className={`${vollkorn.className} text-4xl lg:text-9xl font-extrabold text-slate-800 dark:text-slate-200 mb-6 my-20`}
-          >
-            Nuestro Equipo
-          </h1>
-          <p className="text-xl text-gray-600 dark:text-gray-300 max-w-3xl mx-auto mb-8">
-            Nuestro equipo está compuesto por personas que trabajan con un objetivo en mente: proporcionar soluciones que impulsen <span className="font-bold"> el éxito de tu negocio.</span>
-          </p>
-          <div className="grid lg:grid-cols-1">
-            <section className="mb-8">
-              <Image
-                src="about/teclado-about.svg"
-                alt="Ícono del Equipo"
-                width={300}
-                height={300}
-                className="rounded-full border border-slate-300 dark:border-slate-500 mx-auto shadow-md"
-              />
-            </section>
-          </div>
-        </div>
-
-        {/* Nuestra Misión */}
-        <div className="text-center mb-12 py-12">
+      <div className="mx-auto px-6 max-w-7xl">
+        {/* Developers Section */}
+        <div className="mb-20">
           <h2
-            className={`${vollkorn.className} text-4xl lg:text-6xl font-extrabold text-slate-800 dark:text-slate-200 mb-12 max-w-5xl`}
+            className={`${vollkorn.className} text-4xl lg:text-7xl font-extrabold text-slate-800 dark:text-slate-200 mb-6 my-20`}
           >
-            Nuestra Misión
+            Trabajan en Hexágono
           </h2>
-          <ul className="flex flex-col list-none space-y-4 max-w-2xl items-center justify-center mx-auto pt-12 gap-y-10">
-            <li className="text-stone-700 dark:text-stone-300 flex flex-col justify-center items-center p-6 shadow-md border border-slate-200 dark:border-slate-700 rounded-lg mx-4 lg:mx-0 bg-white dark:bg-card">
-              <User className="text-stone-500 h-20 w-20" />
-              <section className="pt-10 pb-20">
-                <span
-                  className={`${vollkorn.className} text-2xl lg:text-4xl font-extrabold text-stone-700 dark:text-stone-300 mb-6`}
+          <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-8 my-20 ">
+            {developers.map((dev, index) => (
+              <div
+                key={index}
+                className=""
+              >
+                <Image
+                  src={dev.image}
+                  alt={dev.name}
+                  width={150}
+                  height={150}
+                  className="rounded-full mx-auto mb-4 border-4 border-slate-300 dark:border-slate-500"
+                />
+                <h3
+                  className={`${exo2.className} text-2xl font-semibold text-slate-800 dark:text-slate-200 mb-2`}
                 >
-                  Compromiso con el Usuario:
-                </span>
-                <p>
-                  Nos dedicamos a entender y satisfacer las necesidades de
-                  nuestros clientes para ofrecer soluciones que realmente hagan 
-                  <span className="font-bold"> la diferencia.</span>
+                  {dev.name}
+                </h3>
+                <p className="text-sm text-slate-600 dark:text-slate-400 mb-3">
+                  {dev.role}
                 </p>
-              </section>
-            </li>
-            <li className="text-stone-700 dark:text-stone-300 flex flex-col justify-center items-center p-6 shadow-md border border-slate-200 dark:border-slate-700 rounded-lg mx-4 lg:mx-0 bg-white dark:bg-card">
-              <Globe className="text-stone-500 h-20 w-20" />
-              <section className="pt-10 pb-20">
-                <span
-                  className={`${vollkorn.className} text-2xl lg:text-4xl font-extrabold text-stone-700 dark:text-stone-300 mb-6`}
-                >
-                  Impacto Global:
-                </span>
-                <p>
-                  Nuestra misión es generar un impacto positivo y duradero a
-                  nivel global, abordando los desafíos del <span className="font-bold">mundo moderno.</span> 
+                <p className="text-sm text-slate-600 dark:text-slate-400 mb-4">
+                  {dev.description}
                 </p>
-              </section>
-            </li>
-            <li className="text-stone-700 dark:text-stone-300 flex flex-col justify-center items-center p-6 shadow-md border border-slate-200 dark:border-slate-700 rounded-lg mx-4 lg:mx-0 bg-white dark:bg-card">
-              <Target className="text-stone-500 h-20 w-20" />
-              <section className="pt-10 pb-20">
-                <span
-                  className={`${vollkorn.className} text-2xl lg:text-4xl font-extrabold text-stone-700 dark:text-stone-300 mb-6`}
-                >
-                  Enfoque en Resultados:
-                </span>
-                <p>
-                  Estamos comprometidos a entregar resultados que superen las
-                  expectativas, utilizando estrategias basadas en datos y
-                  enfoques orientados a <span className="font-bold">objetivos.</span>
-                </p>
-              </section>
-            </li>
-          </ul>
+                <div className="flex justify-center space-x-2 pt-4">
+                  <Link 
+                    href={dev.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200  border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                  >
+                    <FaGithub size={20} /> {/* Icono de Github */}
+                  </Link>
+                  <Link
+                    href={dev.linkedin}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200 border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                  >
+                    <FaLinkedin size={20} />{" "}
+                    {/* Icono de LinkedIn con ExternalLink */}
+                  </Link>
+                  <Link 
+                    href={dev.instagram}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200  border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                  >
+                    <RiInstagramFill size={20} /> {/* Icono de Twitter */}
+                  </Link>
+                  <Link
+                    href={dev.globe}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200 border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                  >
+                    <FaGlobe size={20} /> {/* Icono de Globe */}
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
