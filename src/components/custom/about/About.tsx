bcb8caecf246054e93675843a8c3bdaf10d8e8f1
GRAMAPHENIA <dicoratojuanpablo@gmail.com>
2024-11-13 16:59:10 -0300

    mods

--- src/components/custom/about/About.tsx
+++ src/components/custom/about/About.tsx
@@ -12,8 +12,8 @@ const AboutUs = () => {
       name: "Dylan Server",
       role: "Frontend Developer",
       image: "/clients/avatar-she-another.webp",
-      description: "React y diseño de interfaces de usuario.",
-      github: "https://github.com/Bird",
+      description: "Desarrollo con React y diseño de interfaces de usuario.",
+      github: "https://github.com/BirdieOA",
       linkedin: "https://www.linkedin.com/in/mait%C3%A9-de-le%C3%B3n/",
       instagram: "https://www.instagram.com/birdie.borda/",
       globe: "https://www.hexagono.xyz/",
@@ -22,7 +22,7 @@ const AboutUs = () => {
       name: "Rojo Server",
       role: "Frontend Developer",
       image: "/clients/avatar-he.webp",
-      description: "React y diseño de interfaces de usuario.",
+      description: "Desarrollo con React y diseño de interfaces de usuario.",
       github: "https://github.com/GRAMAPHENIA",
       linkedin: "https://www.linkedin.com/in/dicoratojuanpablo/",
       instagram: "https://www.instagram.com/gramaphenia/",
@@ -51,49 +51,49 @@ const AboutUs = () => {
                   className="rounded-full mx-auto mb-4 border-4 border-slate-300 dark:border-slate-500"
                 />
                 <h3
-                  className={`${exo2.className} text-2xl font-semibold text-slate-800 dark:text-slate-200 mb-2`}
+                  className={`${exo2.className} text-2xl font-semibold text-slate-800 dark:text-slate-200 `}
                 >
                   {dev.name}
                 </h3>
                 <p className="text-sm text-slate-600 dark:text-slate-400 mb-3">
                   {dev.role}
                 </p>
-                <p className="text-sm text-slate-600 dark:text-slate-400 mb-4">
+                <p className="text-sm text-slate-600 dark:text-slate-400 my-10">
                   {dev.description}
                 </p>
-                <div className="flex justify-center space-x-2 pt-4">
+                <div className="flex justify-center space-x-4 pt-4">
                   <Link
                     href={dev.github}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200  border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200  "
                   >
-                    <FaGithub size={20} /> {/* Icono de Github */}
+                    <FaGithub size={25} /> {/* Icono de Github */}
                   </Link>
                   <Link
                     href={dev.linkedin}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200 border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200 "
                   >
-                    <FaLinkedin size={20} />{" "}
+                    <FaLinkedin size={25} />{" "}
                     {/* Icono de LinkedIn con ExternalLink */}
                   </Link>
                   <Link
                     href={dev.instagram}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200  border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200  "
                   >
-                    <RiInstagramFill size={20} /> {/* Icono de Twitter */}
+                    <RiInstagramFill size={25} /> {/* Icono de Twitter */}
                   </Link>
                   <Link
                     href={dev.globe}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200 border p-4 rounded-full bg-slate-500/10 hover:bg-slate-500/20"
+                    className="text-slate-600 dark:text-slate-400 hover:text-hexagon dark:hover:text-slate-200 "
                   >
-                    <FaGlobe size={20} /> {/* Icono de Globe */}
+                    <FaGlobe size={25} /> {/* Icono de Globe */}
                   </Link>
                 </div>
               </div>
